63a68b2fffbc40af2de0e4c8ca9901c2b9853848
devsum <imsumanthc@gmail.com>
2023-05-19 13:37:46 +0530

    updated login validation

--- utils/validation/user.validation.js
+++ utils/validation/user.validation.js
@@ -4,22 +4,24 @@ const user_model = require("../../models/user.model");
 exports.signup_validation = [
     check("name", "name is required").not().isEmpty(),
     check("email", "email is required").not().isEmpty().isEmail().withMessage('enter valid email')
-    .custom(async (value) => {
-        const isExists = await user_model.find({ email: value });
-        if (isExists.length > 0) {
-            throw new Error('A user already exists with this email address');
-        }
-    }),
+    // .custom(async (value) => {
+    //     const isExists = await user_model.find({ email: value });
+    //     if (isExists.length > 0) {
+    //         throw new Error('A user already exists with this email address');
+    //     }
+    // }),
+    ,
     check("password", "password is required").not().isEmpty().isLength({min: 6}).withMessage('Password Length must be at least 6 characters'),
 ];
 
 exports.login_validation = [
     check("email", "email is required").not().isEmpty().isEmail().withMessage('enter valid email')
-    .custom(async (value) => {
-        const isExists = await user_model.find({ email: value });
-        if (isExists.length == 0) {
-            throw new Error('user not found with email ' + value);
-        }
-    }),
+    // .custom(async (value) => {
+    //     const isExists = await user_model.find({ email: value });
+    //     if (isExists.length == 0) {
+    //         throw new Error('user not found with email ' + value);
+    //     }
+    // }),
+    ,
     check("password", "password is required").not().isEmpty().isLength({min: 6}).withMessage('Password Length must be at least 6 characters'),
 ];
